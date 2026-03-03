e86d25fae50b255d7eb162c82b002c928963d378
Bruno Ferreira <bruno.ferreira@ifmg.edu.br>
2026-03-03 11:40:14 -0300

    02 - criando a entidade.

--- src/main/java/br/ifmg/produto1_2026/Produto12026Application.java
+++ src/main/java/br/ifmg/produto1_2026/Produto12026Application.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Produto12026Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Produto12026Application.class, args);
+
+        SpringApplication.run(Produto12026Application.class, args);
 	}
 
 }
